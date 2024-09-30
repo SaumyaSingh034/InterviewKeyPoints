c506ff5d1d79afbb8a61aa2b1eca05604ec61aaa
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-30 11:07:00 +0530

    Practice Rest Assured

---
+++ src/main/java/POJO/Elements.java
@@ -0,0 +1,41 @@
+package POJO;
+
+public class Elements {
+    private String category;
+    private String author;
+    private String title;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    private Double price;
+}
---
+++ src/main/java/POJO/Interview1POJO.java
@@ -0,0 +1,13 @@
+package POJO;
+
+public class Interview1POJO {
+    private Book store;
+
+    public Book getStore() {
+        return store;
+    }
+
+    public void setStore(Book store) {
+        this.store = store;
+    }
+}
