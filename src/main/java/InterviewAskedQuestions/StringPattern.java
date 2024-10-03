731697b0d37af31c9d91aa144d1a4eba0038b8d4
Saumya Singh <singhsaumyait034@gmail.com>
2024-10-03 22:32:06 +0530

    Pattern questions

---
+++ src/main/java/InterviewAskedQuestions/StringPattern.java
@@ -0,0 +1,11 @@
+package InterviewAskedQuestions;
+
+public class StringPattern {
+
+    public static void main(String[] args){
+        String str = "SAUMYA";
+        for(int i=str.length();i>0;i--){
+            System.out.println(str.substring(0,i));
+        }
+    }
+}
