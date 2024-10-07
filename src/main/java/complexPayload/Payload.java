c7bd024f4f60b3be8a78185faff89e94744417d0
Saumya Singh <singhsaumyait034@gmail.com>
2024-10-07 19:02:56 +0530

    Complex payload

---
+++ src/main/java/complexPayload/Payload.java
@@ -0,0 +1,44 @@
+package complexPayload;
+
+public class Payload {
+    public static String complexJsonRahul(){
+        return "{\r\n" +
+                "  \"dashboard\": {\r\n" +
+                "    \"purchaseAmount\": 1162,\r\n" +
+                "    \"website\": \"rahulshettyacademy.com\"\r\n" +
+                "  },\r\n" +
+                "  \"courses\": [\r\n" +
+                "    {\r\n" +
+                "      \"title\": \"Selenium Python\",\r\n" +
+                "      \"price\": 50,\r\n" +
+                "      \"copies\": 6\r\n" +
+                "    },\r\n" +
+                "    {\r\n" +
+                "      \"title\": \"Cypress\",\r\n" +
+                "      \"price\": 40,\r\n" +
+                "      \"copies\": 4\r\n" +
+                "    },\r\n" +
+                "    {\r\n" +
+                "      \"title\": \"RPA\",\r\n" +
+                "      \"price\": 45,\r\n" +
+                "      \"copies\": 10\r\n" +
+                "    },\r\n" +
+                "     {\r\n" +
+                "      \"title\": \"Appium\",\r\n" +
+                "      \"price\": 36,\r\n" +
+                "      \"copies\": 7\r\n" +
+                "    }\r\n" +
+                "    \r\n" +
+                "    \r\n" +
+                "    \r\n" +
+                "  ]\r\n" +
+                "}\r\n" +
+                "";
+
+
+    }
+
+    public static  String complexJsonAnuj() {
+        return "{\"store\":{\"book\":[{\"category\":\"reference\",\"author\":\"NigelRees\",\"title\":\"SayingsoftheCentury\",\"price\":8.95},{\"category\":\"fiction\",\"author\":\"EvelynWaugh\",\"title\":\"SwordofHonour\",\"price\":12.99},{\"category\":\"fiction\",\"author\":\"HermanMelville\",\"title\":\"MobyDick\",\"isbn\":\"0-553-21311-3\",\"price\":8.99},{\"category\":\"fiction\",\"author\":\"J.R.R.Tolkien\",\"title\":\"TheLordoftheRings\",\"isbn\":\"0-395-19395-8\",\"price\":22.99}],\"bicycle\":{\"color\":\"red\",\"price\":19.95}},\"expensive\":10}";
+    }
+}
