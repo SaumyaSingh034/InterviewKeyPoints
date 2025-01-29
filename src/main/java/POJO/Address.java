1a6a286402307de38531d094317e566d47a81317
Saumya Singh <singhsaumyait034@gmail.com>
2025-01-29 06:56:00 +0530

    pojo

---
+++ src/main/java/POJO/Address.java
@@ -0,0 +1,6 @@
+package POJO;
+
+public class Address {
+    private String city;
+    private String zipCode;
+}
---
+++ src/main/java/POJO/UserPojo.java
@@ -0,0 +1,9 @@
+package POJO;
+
+public class UserPojo {
+    private int userId;
+    private String name;
+    private String email;
+    private Address address;
+
+}
